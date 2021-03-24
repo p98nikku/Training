61dfd5fb03c192ec068a806b65d7e5e1a84768e8
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-24 17:47:43 +0530

    new commit

---
+++ Date-24/RealTime3/RealTime3/Calc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealTime3
+{
+    public class Calc
+    {
+        public delegate int calculator(int num1, int num2);
+        public int addition(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        public int substraction(int num1,int num2)
+        {
+            return num1 - num2;
+        }
+        public int multiplication(int num1,int num2)
+        {
+            return num1 * num2;
+        }
+        public int division(int num1,int num2)
+        {
+            return num1 / num2;
+        }
+        public void Display(calculator d, int num1, int num2)
+        {
+            Console.WriteLine(d(num1, num2));
+        }
+    }
+}
+    
+        
+    
+
---
+++ Date-24/RealTime3/RealTime3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static RealTime3.Calc;
+
+namespace RealTime3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+            Calc.calculator D = c.multiplication;
+            c.Display(D, 12, 12);
+
+        }
+    }
+}
