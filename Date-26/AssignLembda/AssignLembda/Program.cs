8d9d501f87e73d291248c1099e7ecb10fa8fc0d0
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-03-26 17:45:44 +0530

    new commit

---
+++ Date-26/AssignLembda/AssignLembda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AssignLembda
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+          string s1 = "Neetika Paliwal a ss asd a a a abs dd ";
+
+            string[] StringArray = s1.Split(' ');
+
+
+            Console.WriteLine("Using LINQ");
+            var o = from r in StringArray where r.Equals("a") select r;
+            Console.WriteLine(o.Count());
+
+            Console.WriteLine("Using Lambda Expression");
+            var a = StringArray.Where(r => r.Equals("a"));
+            Console.WriteLine(a.Count());
+
+        }
+    }
+}
